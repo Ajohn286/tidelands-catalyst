8f54ff5d7d609d094b6edf3d649a02a9205371fc
Austin Johnson <austinjohnson@Austins-MBP.lan>
2024-05-31 13:56:25 -0400

    new try

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -15,20 +15,21 @@ import {
   SlideshowSlide,
 } from '~/components/ui/slideshow';
 
-import SlideshowBG from './slideshow-bg-01.jpg';
-
 export const Hero = () => (
   <Slideshow>
     <SlideshowContent>
       <SlideshowSlide>
         <div className="relative">
-          <Image
-            alt="an assortment of brandless products against a blank background"
-            className="absolute -z-10 object-cover"
-            fill
-            priority
-            sizes="(max-width: 1536px) 100vw, 1536px"
-            src={Raider}/>
+          <div className="absolute inset-0 -z-10">
+            <Image
+              alt="an assortment of brandless products against a blank background"
+              className="object-cover"
+              layout="fill"
+              priority
+              sizes="(max-width: 1536px) 100vw, 1536px"
+              src={Raider}
+            />
+          </div>
 
           <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
             <h2 className="text-5xl font-black lg:text-6xl">25% Off Sale</h2>
@@ -42,43 +43,53 @@ export const Hero = () => (
         </div>
       </SlideshowSlide>
       <SlideshowSlide>
-        <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
+        <div className="relative">
+          <div className="absolute inset-0 -z-10">
             <Image
               alt="an assortment of brandless products against a blank background"
-              className="absolute -z-10 object-cover"
-              fill
+              className="object-cover"
+              layout="fill"
               priority
               sizes="(max-width: 1536px) 100vw, 1536px"
-              src={Cam}/>
+              src={Cam}
+            />
+          </div>
 
-          <h2 className="text-5xl font-black lg:text-6xl">Great Deals</h2>
-          <p className="max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-          <Button asChild className="w-fit">
-            <a href="/#">Shop now</a>
-          </Button>
+          <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
+            <h2 className="text-5xl font-black lg:text-6xl">Great Deals</h2>
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+            </p>
+            <Button asChild className="w-fit">
+              <a href="/#">Shop now</a>
+            </Button>
+          </div>
         </div>
       </SlideshowSlide>
       <SlideshowSlide>
-        <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
+        <div className="relative">
+          <div className="absolute inset-0 -z-10">
             <Image
               alt="an assortment of brandless products against a blank background"
-              className="absolute -z-10 object-cover"
-              fill
+              className="object-cover"
+              layout="fill"
               priority
               sizes="(max-width: 1536px) 100vw, 1536px"
-              src={RPS}/>
+              src={RPS}
+            />
+          </div>
 
-          <h2 className="text-5xl font-black lg:text-6xl">Low Prices</h2>
-          <p className="max-w-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-          <Button asChild className="w-fit">
-            <a href="/#">Shop now</a>
-          </Button>
+          <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
+            <h2 className="text-5xl font-black lg:text-6xl">Low Prices</h2>
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+            </p>
+            <Button asChild className="w-fit">
+              <a href="/#">Shop now</a>
+            </Button>
+          </div>
         </div>
       </SlideshowSlide>
     </SlideshowContent>
@@ -90,3 +101,4 @@ export const Hero = () => (
     </SlideshowControls>
   </Slideshow>
 );
+
