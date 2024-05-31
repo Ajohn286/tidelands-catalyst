c9fdf01a1206a1f1a29d19a33e371df390ac3f93
Austin Johnson <austinjohnson@Austins-MBP.lan>
2024-05-31 09:16:59 -0400

    code and font changes

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -22,14 +22,6 @@ export const Hero = () => (
     <SlideshowContent>
       <SlideshowSlide>
         <div className="relative">
-          <Image
-            alt="Spearo"
-            className="absolute -z-10 object-cover"
-            fill
-            priority
-            sizes="(max-width: 1536px) 100vw, 1536px"
-            src={Imag4}
-          />
           <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
             <Image
               alt="Spearo"
@@ -37,9 +29,9 @@ export const Hero = () => (
               fill
               priority
               sizes="(max-width: 1536px) 100vw, 1536px"
-              src={Image3}
+              src={Image2}
             />
-            <h2 className="text-5xl font-black lg:text-6xl">25% Off Sale</h2>
+            <h2 className="text-5xl font-white lg:text-6xl">25% Off Sale</h2>
             <p className="max-w-xl">
               Site wide sales on all orders over $300.
             </p>
@@ -51,15 +43,15 @@ export const Hero = () => (
       </SlideshowSlide>
       <SlideshowSlide>
         <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
-        <Image
+          <Image
             alt="Spearo"
             className="absolute -z-10 object-cover"
             fill
             priority
             sizes="(max-width: 1536px) 100vw, 1536px"
-            src={Image2}
-          />
-          <h2 className="text-5xl font-black lg:text-6xl">Cam Kirkconnells Trips to the Tropics</h2>
+            src={Image3}
+            />
+          <h2 className="text-5xl font-white lg:text-6xl">Cam Kirkconnells Trips to the Tropics</h2>
           <p className="max-w-xl">
             Finding Big Tuna and Oneself
           </p>
@@ -78,7 +70,7 @@ export const Hero = () => (
             sizes="(max-width: 1536px) 100vw, 1536px"
             src={Image5}
           />
-          <h2 className="text-5xl font-black lg:text-6xl">Life of the Bajau</h2>
+          <h2 className="text-5xl font-white lg:text-6xl">Life of the Bajau</h2>
           <p className="max-w-xl">
             What 2000 years of freediving can teach us.
           </p>
